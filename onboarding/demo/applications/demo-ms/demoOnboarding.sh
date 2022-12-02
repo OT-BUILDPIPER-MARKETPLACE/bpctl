d64db89b7276f847f3c66a2571c3f6bcd59ba688
Sandeep <sandeep@opstree.com>
2022-12-02 16:51:58 +0530

    Added flow for job templates

--- onboarding/demo/applications/demo-ms/demoOnboarding.sh
+++ onboarding/demo/applications/demo-ms/demoOnboarding.sh
@@ -31,5 +31,21 @@ function demoEnvironments() {
     pause
 }
 
+function demoJobTemplates() {
+    printDigitalMessage "One last thing to do before MS onboarding"
+    printDigitalMessage "Job Templates!!!!"
+    printDigitalMessage "Job templates are the way you can bring governance & standardization in your org"
+    printDigitalMessage "Some of the examples"
+    printDigitalMessage "We will onboard 2 job templates in this demo"
+    printDigitalMessage "Extensive DevSecOps CI, having checks as Licence analysis, Cred scanning in code, Image scanning..."
+    printDigitalMessage "Basic CI with only buidling docker image and publishing it"
+    bpctl apply -f job-templates/demo-ms.bp.yaml
+    bpctl apply -f job-templates/extensive-ci.bp.yaml
+    printDigitalMessage "Want to view the job templates"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/10/job-templates"
+    pause
+}
+
 welcomeMessage
 demoEnvironments
+demoJobTemplates
