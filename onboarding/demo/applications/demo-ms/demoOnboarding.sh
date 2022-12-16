4e031b85654633641d35ee2d0cc247e10a92e95c
Sandeep <sandeep@opstree.com>
2022-12-16 19:30:22 +0530

    Updated demo script

--- onboarding/demo/applications/demo-ms/demoOnboarding.sh
+++ onboarding/demo/applications/demo-ms/demoOnboarding.sh
@@ -31,7 +31,7 @@ function demoEnvironments() {
     bpctl apply -f environments/dev.bp.yaml
     bpctl apply -f environments/qa.bp.yaml
     printDigitalMessage "Want to view the environments?"
-    printDigitalMessage "http://demo.buildpiper.in:56080/application/10/env/list"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/11/env/list"
     pause
 }
 
@@ -42,10 +42,10 @@ function demoJobTemplates() {
     printDigitalMessage "We will on-board 2 job templates in this demo"
     printDigitalMessage "Basic CI with only buidling docker image and publishing it"
     printDigitalMessage "Extensive DevSecOps CI, having checks as Licence analysis, Cred scanning in code, Image scanning..."
-    bpctl apply -f job-templates/demo-ms.bp.yaml
-    bpctl apply -f job-templates/extensive-ci.bp.yaml
+    bpctl apply -f job-templates/ms-basic-ci.bp.yaml
+    bpctl apply -f job-templates/devsecops-ci.bp.yaml
     printDigitalMessage "Want to view the job templates?"
-    printDigitalMessage "http://demo.buildpiper.in:56080/application/10/job-templates"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/11/job-templates"
     pause
 }
 
@@ -58,7 +58,9 @@ function demoMSOnBoarding() {
     printDigitalMessage "MS Meta info definition"
     printDigitalMessage "MS environment meta info definition"
     printDigitalMessage "MS CI configurations"
-    printDigitalMessage "MS CD configuratinos"
+    printDigitalMessage "MS CD configurations"
+    pause
+    
     printDigitalMessage "Enough of talks let's start with emp-ui MS"
     printDigitalMessage "emp-ui MS meta info..."
     bpctl apply -f services/emp-ui/emp-ui-service-def.bp.yaml
@@ -68,12 +70,17 @@ function demoMSOnBoarding() {
     bpctl apply -f services/emp-ui/env/dev/ci/emp-ui-ci.bp.yaml
     printDigitalMessage "emp-ui dev env CD configurations..."
     bpctl apply -f services/emp-ui/env/dev/cd/emp-ui-dev.bp.yaml
+    printDigitalMessage "Want to view the onboarded MS"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/11/service/listing"
+    pause
+
     printDigitalMessage "emp-ui qa env meta info..."
     bpctl apply -f services/emp-ui/env/qa/emp-ui-qa.bp.yaml
-    printDigitalMessage "emp-ui qa env CI configurations..."
-    bpctl apply -f services/emp-ui/env/qa/ci/emp-ui-ci.bp.yaml
     printDigitalMessage "emp-ui qa env CD configurations..."
     bpctl apply -f services/emp-ui/env/qa/cd/emp-ui-qa.bp.yaml
+    printDigitalMessage "Want to view the onboarded MS"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/11/service/listing"
+    pause
 
     printDigitalMessage "Great so emp-ui is onboarding with 2 env dev & qa"
     printDigitalMessage "Let's move to emp-salary MS"
@@ -81,17 +88,23 @@ function demoMSOnBoarding() {
     bpctl apply -f services/emp-salary/emp-salary-service-def.bp.yaml
     printDigitalMessage "emp-salary dev env meta info..."
     bpctl apply -f services/emp-salary/env/dev/emp-salary-dev.bp.yaml 
+    printDigitalMessage "emp-salary dev env CI configurations..."
+    bpctl apply -f services/emp-salary/env/dev/ci/emp-salary-dev-ci.bp.yaml
+    printDigitalMessage "emp-salary dev env CD configurations..."
+    bpctl apply -f services/emp-salary/env/dev/cd/emp-salary-dev-cd.bp.yaml
+
     printDigitalMessage "emp-salary qa env meta info..."
     bpctl apply -f services/emp-salary/env/qa/emp-salary-qa.bp.yaml
-
+    printDigitalMessage "emp-salary qa env CD configurations..."
+    bpctl apply -f services/emp-salary/env/qa/cd/emp-salary-qa-cd.bp.yaml
     printDigitalMessage "Want to view the onboarded MS"
-    printDigitalMessage "http://demo.buildpiper.in:56080/application/10/service/listing"
+    printDigitalMessage "http://demo.buildpiper.in:56080/application/11/service/listing"
     pause
 }
 
 function codeStructure() {
     printDigitalMessage "Now you would be wondering how all this managed?"
-    printDigitalMessage "How the code tructure looks like"
+    printDigitalMessage "How the code structure looks like"
     tree
     pause
 }
